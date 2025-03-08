4273e2c9b232877d592a7657a61d9815bcd9343d
AbKadar Chaudhari <113678495+abdulkadir4self@users.noreply.github.com>
2025-03-08 12:03:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const xlsx = require("xlsx");
 
 app.use(bodyParser.json());
 app.use(cors());
-// app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 
 // property route here
